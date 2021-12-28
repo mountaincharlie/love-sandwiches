96c0b87dcc38c16e85721f3ed935270690ac51bb
Charlie <ciharland@hotmail.co.uk>
2021-12-28 12:17:18 +0000

    Add validation function to check 6 values are provided by the user

--- run.py
+++ run.py
@@ -30,14 +30,33 @@ data = sales.get_all_values()
 # getting the sales data
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user. Calls validate_data()
     """
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60")
 
     data_str = input("Enter you data here: ")
-    print(f"The data provided is {data_str}")
+
+    # splitting at the commas
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the Try, converts all string values into integers.
+    Raises ValueError if string cannot be converted into int,
+    or if there arent exactly 6 values.
+    Called by get_sales_data()
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 
 get_sales_data()
