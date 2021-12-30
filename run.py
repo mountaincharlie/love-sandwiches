9b355ce1fbe892bcb3dba90d8abb4fcafc37c263
Charlie <ciharland@hotmail.co.uk>
2021-12-30 13:29:51 +0000

    Add requiremenets for deployment

--- run.py
+++ run.py
@@ -3,7 +3,6 @@
 # imports
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 # list of APIs the program should access in order to run
 SCOPE = [
@@ -21,13 +20,6 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 # accessing the google sheet
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-# accessing the worksheet in the google sheet
-# sales = SHEET.worksheet('sales')
-
-# getting all the values from the sales worksheet
-# data = sales.get_all_values()
-
-
 # getting the sales data
 def get_sales_data():
     """
@@ -43,7 +35,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60")
 
-        data_str = input("Enter you data here: ")
+        data_str = input("Enter you data here:\n")
 
         # splitting at the commas
         sales_data = data_str.split(",")
